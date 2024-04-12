6653754a37a980221d88d3ac9d515bd82b6a0fda
Bititude Technologies <bititudetechnologies@Wolf.local>
2024-04-12 16:07:19 +0530

    Todo App: Commiting After Review Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
+import "./App.css";
 
-
-import TodoForm from './components/todoform/todoForm';
+import TodoForm from "./components/todoform/todoForm";
 
 function App() {
   return (
     <div className="App">
-      <div className='mainBody'>
-        <h2 className='heading'>Get Things Done!</h2>
-        <TodoForm/>
+      <div className="mainBody">
+        <h2 className="heading">Get Things Done!</h2>
+        <TodoForm />
       </div>
     </div>
   );
